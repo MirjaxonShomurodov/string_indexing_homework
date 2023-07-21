346cf74a3be6963dc51b3d26a8883a750f959dfd
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-21 18:08:22 +0500

    1 misol

--- Index01.py
+++ Index01.py
@@ -1,9 +1,12 @@
+# def main(s):
+#     """
+#     The string variable s is given. return the first character.
+#     Args:
+#         s(str): parameter
+#     Returns:
+#         str: answer
+#     """
 def main(s):
-    """
-    The string variable s is given. return the first character.
-    Args:
-        s(str): parameter
-    Returns:
-        str: answer
-    """
-    return
+    a="python"
+    return a[0]
+print(main("python"))

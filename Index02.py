cf20c459913d96cccfd2d488e9aa420897060591
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-21 18:05:18 +0500

    2 misol

--- Index02.py
+++ Index02.py
@@ -1,9 +1,13 @@
+# def main(s):
+#     """
+#     The string variable s is given. return the second character.
+#     Args:
+#         s(str): parameter
+#     Returns:
+#         str: answer
+#     """
+
 def main(s):
-    """
-    The string variable s is given. return the second character.
-    Args:
-        s(str): parameter
-    Returns:
-        str: answer
-    """
-    return
+    a="python"
+    return a[2]
+print(main("python"))

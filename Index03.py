e6ff3754d173da93f79bdd61811be2ba2b229ea8
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-21 19:23:23 +0500

    3 misol

--- Index03.py
+++ Index03.py
@@ -1,9 +1,13 @@
+# def main(s):
+#     """
+#     The string variable s is given. return the last character.
+#     Args:
+#         s(str): parameter
+#     Returns:
+#         str: answer
+#     """
+#     return
 def main(s):
-    """
-    The string variable s is given. return the last character.
-    Args:
-        s(str): parameter
-    Returns:
-        str: answer
-    """
-    return
+    a="python"
+    return a[5]
+print(main("python"))

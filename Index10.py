67493af9114568803e94d4d69d02b3d709956a82
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-22 06:17:59 +0500

    10 misol

--- Index10.py
+++ Index10.py
@@ -6,4 +6,26 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    a1=s%10
+    s//=10
+
+    a2=s%10
+    s//=10
+
+    a3=s%10
+    s//=10
+
+    a4=s%10
+    s//10
+
+    a5=s%10
+    s//=10
+
+    a6=s
+    m=a1+a2+a3+a4+a5+a6
+    s=int("10920")
+    return m
+print(main(int("10920")))
+
+   
+   
